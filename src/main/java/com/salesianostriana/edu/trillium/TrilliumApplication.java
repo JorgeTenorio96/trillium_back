a41f58be760e8c99e8268b3d3a4cbf887e1ab74b
JorgeTenorio96 <jorgetenorio96@gmail.com>
2023-02-09 14:17:45 +0100

    Preparando el terreno...

---
+++ src/main/java/com/salesianostriana/edu/trillium/TrilliumApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.trillium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrilliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrilliumApplication.class, args);
+	}
+
+}
