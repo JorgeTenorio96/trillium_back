cf8886d6251ae03da96eb1684be838847beb8c46
JorgeTenorio96 <jorgetenorio96@gmail.com>
2023-03-01 12:19:09 +0100

    import sql actualizado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,5 @@
 insert into user_entity (id, username, password, full_Name, email, account_non_expired, account_non_locked, credentials_non_expired, enabled, roles, created_at, last_password_change_at) values('751c3611-8882-5751-9f0d-d216dd047ebc', 'user1', '{bcrypt}$2a$12$/p0xgyhb5KfBK4.FDrKnAuQe1oBvksF3x1qPFaVUCqCd4ZU/30OsK','User1','user1@gmail.com',   true, true, true, true, 'USER', '2021-09-26', '2021-09-26');
 
 insert into post (id, title, image) values ('1', 'Meme', 'https://www.si.com/.image/t_share/MTk1MTE2NTQzODg0MTQyMzA2/meme3.jpg');
+insert into post (id, title, image) values ('2', 'Un tio mu feo', 'https://gamepress.gg/grandorder/sites/grandorder/files/2023-01/371_Tezcatlipoca_3.png');
+insert into post (id, title, image) values ('3', 'Dracula', 'https://1.bp.blogspot.com/-O2R4WdgOB6c/YKQZkWbTs4I/AAAAAAABWc4/GyGEvuKGOJ0nrUs14q0Sj78a997TPopZACLcBGAsYHQ/s1500/HOTEL%2BTRANSYLVANIA%2B%2BTRANSFORMANIA.jpg');
